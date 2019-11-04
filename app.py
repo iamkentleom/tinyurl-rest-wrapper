ba9805bf6578b8a465325948a7e691b58475ea8f
iamkentleom <leomakibulan@gmail.com>
2019-11-04 22:20:54 +0800

    initial docs page

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import requests as req
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 @app.route('/shorten')
 def shorten_url():
@@ -18,4 +26,4 @@ def shorten_url():
     return jsonify(res)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
